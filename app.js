f7038ef27c86a66ce46ce642c590b5dd6433d9fc
monkqvq <xiemengke1995@gmail.com>
2022-12-17 15:03:18 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const Todo = require('./models/todo');
 
 if (process.env.NODE_ENV !== 'production') {
@@ -28,15 +29,29 @@ db.once('open', () => {
   console.log('mongodb connected!');
 });
 
-// set app view engine
+// set app
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
     .then((todos) => res.render('index', { todos }))
+    .catch((error) => console.log(error));
+});
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new');
+});
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name; // 從 req.body 拿出表單裡的 name 資料
+
+  return Todo.create({ name }) // 存入資料庫
+    .then(() => res.redirect('/'))
     .catch((error) => console.log(error));
 });
 
