d28c35f68058e79705410acbc3bc9397d93a70b8
monkqvq <xiemengke1995@gmail.com>
2022-12-17 15:21:51 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -55,6 +55,15 @@ app.post('/todos', (req, res) => {
     .catch((error) => console.log(error));
 });
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch((error) => console.log(error));
+});
+
 // start and listen on the Express server
 app.listen(3000, () => {
   console.log(`App is running on http://localhost:${port}`);
